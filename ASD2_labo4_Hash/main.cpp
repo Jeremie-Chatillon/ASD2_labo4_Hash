02f064454d921dd59b82f7318177f6592112c927
Perceval <dnamella1@hotmail.com>
2017-12-15 13:56:48 +0100

    Ecriture dnas un csv

--- ASD2_labo4_Hash/main.cpp
+++ ASD2_labo4_Hash/main.cpp
@@ -9,7 +9,6 @@
 
 #define FILENAME "liste1.txt"
 //#define FILENAME "liste2.txt"
-#define MAX_LOAD_FACTOR 0.5f
 
 #include <cstdlib>
 #include <iostream>
@@ -27,38 +26,50 @@
 
 #include "DirectoryWithoutAVS.h"
 
+float MAX_LOAD_FACTOR = 0.1f;
+
 template <typename Type>
 void test(std::string filename) {
+    
     std::cout << "------------------------------------------------------" << std::endl;
     std::cout << "Test for " << typeid(Type).name() << std::endl;
     std::cout << "------------------------------------------------------" << std::endl;
-    HashTableTester<Type> tester(filename);
+    HashTableTester<Type> tester(filename, MAX_LOAD_FACTOR);
+    std::ostringstream oss;
+        oss << typeid(Type).name() << "," << MAX_LOAD_FACTOR << ",";
+    tester.saveStats(oss.str());    
+    
     std::cout << "Insertion: " << std::endl;
     tester.displayInsertionStats();
     std::cout << "Distribution: " << std::endl;
     tester.displayDistributionStats();
     tester.performSearch();
     std::cout << "Search: " << std::endl;
+    
     tester.displaySearchStats();
     std::cout << std::endl;
+     
+    tester.saveStats("\n");
 }
 
 int main() {
-
     std::string filename(FILENAME);
-    
-    //PART 1
-    test<DirectoryInt>(filename);
-    test<DirectoryLong>(filename);
-    test<DirectoryPol<2>>(filename);    //polynomial with z =  2
-    test<DirectoryPol<31>>(filename);   //polynomial with z = 31
-    test<DirectoryPol<37>>(filename);   //polynomial with z = 37
-    test<DirectoryStl>(filename);
-    test<DirectorySha256>(filename);
-    test<DirectoryCity>(filename);
-    
+    for(int i = 1; i < 10; i++) {
+        //PART 1
+        test<DirectoryInt>(filename);
+        /*
+        test<DirectoryLong>(filename);
+        test<DirectoryPol<2>>(filename);    //polynomial with z =  2
+        test<DirectoryPol<31>>(filename);   //polynomial with z = 31
+        test<DirectoryPol<37>>(filename);   //polynomial with z = 37
+        test<DirectoryStl>(filename);
+        test<DirectorySha256>(filename);
+        test<DirectoryCity>(filename);
+        */
+        MAX_LOAD_FACTOR += 0.1;
+    }
     //PART 2
-    test<DirectoryWithoutAVS>(filename);
+    //test<DirectoryWithoutAVS>(filename);
     
     return EXIT_SUCCESS;
     
